de4b8edc2dc3f106cd855850be216096b4f3b070
Jason <Jason@Miles-workPC>
2014-07-14 23:45:05 +0800

    完成天气预报

--- src/com/miles/maipu/luzheng/NormalIndexActivity.java
+++ src/com/miles/maipu/luzheng/NormalIndexActivity.java
@@ -1,10 +1,20 @@
 package com.miles.maipu.luzheng;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.Map;
 
-public class NormalIndexActivity extends Activity
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.miles.maipu.net.HttpPostUtil;
+import com.miles.maipu.util.AbsBaseActivity;
+import com.miles.maipu.util.JSONUtil;
+
+public class NormalIndexActivity extends AbsBaseActivity
 {
 
 	@Override
@@ -12,6 +22,39 @@ public class NormalIndexActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_index);
+		new getweather().execute("");
+	}
+	
+	class getweather extends AsyncTask<String, String, String>
+	{
+
+		@Override
+		protected String doInBackground(String... params)
+		{
+			// TODO Auto-generated method stub
+			return HttpPostUtil.GetWeather();
+//			return null;
+		}
+
+		@SuppressLint("ShowToast")
+		@Override
+		protected void onPostExecute(String result)
+		{
+			// TODO Auto-generated method stub
+			Map map = (Map)JSONUtil.getMapFromJson(result).get("weatherinfo");
+			Toast.makeText(mContext, map.toString(), 0).show();
+			((TextView)findViewById(R.id.text_weather)).setText(map.get("weather1").toString());
+			((TextView)findViewById(R.id.text_temp)).setText(map.get("temp1").toString());
+			int imgid = R.drawable.a00;
+			imgid+=Integer.parseInt(map.get("img1").toString());
+			((ImageView)findViewById(R.id.image_weather)).setImageResource(imgid);
+			
+			
+			super.onPostExecute(result);
+		}
+		
+		
+		
 	}
 
 	@Override
---
+++ src/com/miles/maipu/net/HttpPostUtil.java
@@ -0,0 +1,147 @@
+package com.miles.maipu.net;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.ByteArrayBuffer;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+
+/**
+ * 网络通信底层方法类，通过本类实现网络数据交互和Json的数据解析
+ * 
+ * @author 牛程
+ * @version 1.0
+ * @since 2013/04/12
+ * */
+public class HttpPostUtil
+{	
+	/** 通用接口 */
+
+	public static String Url = "http://micro.nestcms.com/router.api";
+	public static String WeatherUrl = "http://m.weather.com.cn/atad/101191101.html";
+
+	public static String CheckUrl = "http://61.160.245.48:10003/AppManager.aspx?Appid=2";
+
+	@SuppressLint("SimpleDateFormat")
+	public static String GetWeather()
+	{
+
+		String result = "";
+		InputStream is = null;
+		HttpGet httpRequest = new HttpGet(WeatherUrl);
+		try
+		{
+			HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest);
+			if (httpResponse.getStatusLine().getStatusCode() == 200)
+			{ // 正确
+
+				is = httpResponse.getEntity().getContent();
+				byte[] data = new byte[1024];
+				int n = -1;
+				ByteArrayBuffer buf = new ByteArrayBuffer(10 * 1024);
+				while ((n = is.read(data)) != -1)
+					buf.append(data, 0, n);
+				result = new String(buf.toByteArray(), HTTP.UTF_8);
+				is.close();
+					
+				return result;
+			}
+			else
+			{
+				Log.v("tip==", "error response code");
+				return "";
+			}
+		}
+		catch (Exception e)
+		{
+			Log.e("error==", "" + e.getMessage());
+			return "";
+		}
+	}
+
+	/**
+	 * post网络交互
+	 * 
+	 * @param serlizetype
+	 *            接口类型
+	 * @param requestString
+	 *            需向网络发送的参数字符串
+	 * @return 返回Json解析后的数据对象
+	 * */
+	public static Object httpUrlConnection(String requestString)
+	{
+		Object objBack = null;
+				
+//		if(!CheckApp())
+//		{
+//			return null;
+//		}
+		//System.out.println("send info 2 web:  " + requestString);
+		try
+		{
+			// 建立连接
+			URL url = new URL(Url);
+
+			HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+			// 设置连接属性
+			httpConn.setDoOutput(true); // 使用 URL 连接进行输出
+			httpConn.setDoInput(true); // 使用 URL 连接进行输入
+			httpConn.setUseCaches(false); // 忽略缓存
+			httpConn.setRequestMethod("POST"); // 设置URL请求方法
+
+			// 设置请求属性
+			// 获得数据字节数据，请求数据流的编码，必须和下面服务器端处理请求流的编码一致
+			byte[] requestStringBytes = requestString.getBytes();
+			httpConn.setRequestProperty("Content-length", "" + requestStringBytes.length);
+			httpConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			httpConn.setRequestProperty("Connection", "Keep-Alive"); // 维持长连接
+			httpConn.setRequestProperty("Charset", "UTF-8");
+			httpConn.setConnectTimeout(10000);
+			httpConn.setReadTimeout(10000);
+
+			// 建立输出流，并写入数据
+			OutputStream outputStream = httpConn.getOutputStream();
+			outputStream.write(requestStringBytes);
+			outputStream.close();
+
+			// 获得响应状态
+			int responseCode = httpConn.getResponseCode();
+			if (HttpURLConnection.HTTP_OK == responseCode)
+			{
+				// 当正确响应时处理数据
+				StringBuffer sb = new StringBuffer();
+				String readLine;
+				BufferedReader responseReader;
+				// 处理响应流，必须与服务器响应流输出的编码一致
+				responseReader = new BufferedReader(new InputStreamReader(httpConn.getInputStream(), "UTF-8"));
+				while ((readLine = responseReader.readLine()) != null)
+				{
+					sb.append(readLine).append("\n");
+				}
+				responseReader.close();
+				//System.out.println("Jason:  " + sb.toString());
+
+				return sb.toString().equals("")?"{}":sb.toString();
+
+			}
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			return "{\"Status\":\"false\",\"msg\":\"网络连接失败...\"}";
+		}
+		return objBack;
+	}
+
+	
+}
